7c9d6e75d339fa30768ced79e48ae6bef56261d8
Homero Benavides <homero.hbr@gmail.com>
2020-02-18 12:12:22 -0600

    Modify hello.py

--- hello.py
+++ hello.py
@@ -1,8 +1,7 @@
 # Example 1. Simple program
 
 # Save input inside a new variable
-input_variable = input("What's your name?")
+input_variable = input("What's your name? \n")
 
 # Use variable
-print(f"Hello {input_variable}")
-
+print(f"Hello, {input_variable}. This is Python!"
